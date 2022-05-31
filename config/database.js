d7958de93084edb7a074d3ce46b08e7dc9830bfb
Kelly Castaños <kelly.castanosx@gmail.com>
2022-05-30 22:14:38 -0400

    second commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://kellycastanos:Castanos1!@cluster0.0jwl8.mongodb.net/gifs?retryWrites=true&w=majority', 
+    'dbName': 'dog-gifs'
+};
